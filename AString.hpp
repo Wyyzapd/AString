959c1a874ea7c6106e3964d836ed326778dc729e
Src Zyl <2417003090@qq.com>
2024-01-28 07:22:05 +0800

    Update AString.hpp

--- AString.hpp
+++ AString.hpp
@@ -1,129 +1,225 @@
 #ifndef _AString_
 #define _AString_
 
+#include <atlstr.h>
+#include <iostream>
 #include <string>
 #include <cstdint>
 
-class AString {
-public:
-	AString();
-	AString(char* __s);
-	AString(std::string& __s);
-	AString(const char* __s);
-	AString(std::uint32_t reserve);
-	AString(AString& other);
-	~AString() throw();
+namespace Math {
+    template <class T>
+    static inline T RoundUp(T value, T alignment) {
+        return (value + alignment - 1) & ~(alignment - 1);
+    }
 
-typedef char* iterator;
+    template <class T>
+    static inline T Max(T a, T b) {
+        return (a > b) ? a : b;
+    }
 
-	AString& Assign(char byte);
-	AString& Assign(AString& __s);
-	AString& Assign(const AString& __s);
-	AString& Assign(char* __s);
-	AString& Assign(const char* __s);
-	AString& Assign(std::string& __s);
-	AString& Assign(const char* __s, const char* __e);
-	AString& AssignLower(const char* __s, const char* __e);
-	AString& AssignUpper(const char* __s, const char* __e);
+    template<class T> struct RemoveReference { using type = T;};
+    template<class T> struct RemoveReference<T&> { using type = T;};
+    template<class T> struct RemoveReference<T&&> { using type = T;};
 
-
-	bool Equals(const char* __s);
-	bool Equals(const AString& other);
-	bool EqualsI(const char* __s);
-	bool EqualsI(const AString& other);
-	bool IsEmpty() const;
-	int  Compare(const char* __s);
-	int  CompareI(const char* __s);
-	bool EndsWith(char c) const;
-	bool EndsWith(const char* __s) const;
-	bool EndsWith(const AString& other) const;
-	bool EndsWithI(char __s) const;
-	bool EndsWithI(const char* __s) const;
-	bool EndsWithI(const AString& other) const;
-	bool BeginsWith(char c) const;
-	bool BeginsWith(const char* __s) const;
-	bool BeginsWith(const AString& other) const;
-	bool BeginsWithI(const char* __s) const;
-	bool BeginsWithI(const AString& other) const;
-
-
-	AString& Format(const char* Format, ...);
-	AString& VFormat(const char* Format, va_list arg);
-
-	void     Clear(void);
-	void     ClearAndFreeMemory(void);
-
-	const char* SetLength(std::uint32_t _size);
-
-	// C-style compatibility
-	char* data() noexcept;
-	const char* c_str() const noexcept;
-	iterator       end() noexcept;
-	iterator       begin() noexcept;
-	const iterator begin() const noexcept;
-	const iterator end() const noexcept;
-	const iterator c_end() const noexcept;
-	std::uint32_t length() const;
-	std::uint32_t size() const;
-
-
-	// append
-	AString& Append(char byte);
-	AString& Append(const char* __s);
-	AString& Append(const AString& other);
-	AString& Append(const std::string& other);
-	AString& Append(const char* __s, std::uint32_t size);
-	AString& AppendFormat(const char* Format, ...);
-
-
-	// operator override
-	AString& operator+=(char byte);
-	AString& operator+=(const AString& other);
-	AString& operator+=(std::string& __s);
-	AString& operator+=(std::wstring& __s);
-	AString& operator+=(const char* __s);
-	AString& operator+ (char byte);
-	AString& operator+ (const AString& other);
-	AString& operator+ (std::string& __s);
-	AString& operator+ (const char* __s);
-	AString& operator= (char byte);
-	AString& operator= (const AString& other);
-	AString& operator= (std::string& __s);
-	AString& operator= (std::wstring& __s);
-	AString& operator= (const char* __s);
-	AString& operator= (const void* __ptr);
-	AString& operator= (std::uint32_t value);
-	bool     operator==(const char* __s) const;
-	bool     operator==(const AString& other) const;
-	bool     operator!=(const AString& other) const;
-	bool     operator!=(const char* __s) const;
-	char*    operator[](std::uint32_t index);
-
-	void ToLower();
-	void ToUpper();
-
-	bool  find(const char* __s);
-	char* Find(const char* __s);
-
-	std::uint64_t Hash();
-private:
-	void  Grow(std::uint32_t _size);
-	void  GrowNoCopy(std::uint32_t _size);
-	void  SetReserved(std::uint32_t _size);
-	bool  StrstrEx(const char* Str1, const char* Str2);
-protected:
-	enum : std::uint32_t { MEM_MUST_BE_FREED_FLAG = 0x00000001 };
-	enum : std::uint32_t { RESERVED_MASK = 0xFFFFFFFE };
-	enum : std::uint64_t { k_offset_basis = 0xcbf29ce484222325ull };
-	enum : std::uint64_t { k_prime = 1099511628211ull };
-
-	char* str;
-	std::uint32_t intlength;
-	std::uint32_t capacity;
 };
 
+template<class T> using RemoveReferenceT = typename Math::RemoveReference<T>::type;
 
 
+class AString{
+public:
+    AString();
+    AString(std::uint32_t _capacity);
+    AString(const AString& string);
+    AString(const std::string& string);
+    AString(const char* string);
+    AString(const char* start, const char* end);
+    ~AString();
 
+    _NODISCARD std::uint32_t      length() const;
+    _NODISCARD bool               IsEmpty() const;
+
+    _NODISCARD char* data();
+    _NODISCARD const char* c_str() const;
+    _NODISCARD const char* c_end() const;
+
+    _NODISCARD char* begin();
+    _NODISCARD const char* c_begin() const;
+    _NODISCARD char* end();
+
+    static const AString& GetEmpty();
+
+    void Assign(const char* string);
+    void Assign(const char* start, const char* end);
+    void Assign(const AString& string);
+    void Assign(AString&& string);
+
+    void clear();
+    void ClearAndFreeMemory();
+    void SetReserved(std::size_t capacity);
+
+    void SetCapacity(std::uint32_t _length);
+
+    _NODISCARD char& operator [] (size_t index);
+    AString& operator = (const char* string);
+    AString& operator = (const AString& string);
+    AString& operator + (char c);
+    AString& operator + (std::uint32_t value);
+    AString& operator + (std::uint64_t value);
+    AString& operator + (const std::string& string);
+    AString& operator + (const char* string);
+    AString& operator + (const AString& string);
+    AString& operator += (char c);
+    AString& operator += (std::uint32_t value);
+    AString& operator += (std::uint64_t value);
+    AString& operator += (const std::string& string);
+    AString& operator += (const char* string);
+    AString& operator += (const AString& string);
+    _NODISCARD bool operator == (const char* other) const;
+    _NODISCARD bool operator == (const AString& other) const;
+    _NODISCARD bool operator != (const char* other) const;
+    _NODISCARD bool operator != (const AString& other) const;
+    _NODISCARD bool operator < (const AString& other) const;
+    _NODISCARD bool operator > (const AString& other) const;
+
+    AString& Append(const AString& string);
+    AString& Append(const char* string, size_t _length);
+    AString& Append(const char* start, const char* end);
+    AString& AppendFormat(_In_z_ _Printf_format_string_ const char* Format, ...);
+
+    _NODISCARD std::int32_t Compare(const AString& other) const;
+    _NODISCARD std::int32_t Compare(const char* other) const;
+    _NODISCARD std::int32_t CompareI(const AString& other) const;
+    _NODISCARD std::int32_t CompareI(const char* other) const;
+    _NODISCARD bool         Equals(const char* other) const;
+    _NODISCARD bool         Equals(const AString& other) const;
+    _NODISCARD bool         EqualsI(const char* other) const;
+    _NODISCARD bool         EqualsI(const AString& other) const;
+
+    _NODISCARD bool         MustBeFreed() const;
+
+    _NODISCARD        std::uint64_t Hash();
+    _NODISCARD        std::uint32_t Hashw32();
+
+    AString& Format(const char* Format,...);
+    AString& VFormat(const char* Format, va_list args);
+
+    _NODISCARD std::int32_t           Scan(_Scanf_s_format_string_ const char* _Format, ...) const;
+    _NODISCARD static std::int32_t    ScanS(const char* Buffer, _Scanf_s_format_string_ const char* _Format, ...);
+
+
+    std::uint32_t               Replace(char from, char to, std::uint32_t maxReplaces = 0);
+    std::uint32_t               Replace(const char* from, const char* to, std::uint32_t maxReplaces = 0);
+
+    void                        ToLower();
+    void                        ToUpper();
+    void                        Trim(std::uint32_t startCharsToTrim, std::uint32_t endCharsToTrim);
+    void                        TrimStart(char charToTrimFromStart);
+    void                        TrimEnd(char charToTrimFromStart);
+
+    _NODISCARD bool        Find(const char* _str);
+    _NODISCARD const char* Find(char c, const char* startPos = nullptr, const char* endPos = nullptr) const;
+    _NODISCARD const char* Find(const char* subString, const char* startPos = nullptr, const char* endPos = nullptr) const;
+    _NODISCARD const char* Find(const AString& subString, const char* startPos = nullptr, const char* endPos = nullptr) const;
+
+    _NODISCARD const char* FindI(char c, const char* startPos = nullptr, const char* endPos = nullptr) const;
+    _NODISCARD const char* FindI(const char* subString, const char* startPos = nullptr, const char* endPos = nullptr) const;
+    _NODISCARD const char* FindI(const AString& subString, const char* startPos = nullptr, const char* endPos = nullptr) const;
+
+    _NODISCARD const char* FindLast(char c, const char* startPos = nullptr, const char* endPos = nullptr) const;
+    _NODISCARD const char* FindLast(const char* subString, const char* startPos = nullptr, const char* endPos = nullptr) const;
+    _NODISCARD const char* FindLast(const AString& subString, const char* startPos = nullptr, const char* endPos = nullptr) const;
+
+    _NODISCARD const char* FindLastI(char c, const char* startPos = nullptr, const char* endPos = nullptr) const;
+    _NODISCARD const char* FindLastI(const char* subString, const char* startPos = nullptr, const char* endPos = nullptr) const;
+    _NODISCARD const char* FindLastI(const AString& subString, const char* startPos = nullptr, const char* endPos = nullptr) const;
+
+    _NODISCARD bool          EndsWith(char c) const;
+    _NODISCARD bool          EndsWith(const char* string) const;
+    _NODISCARD bool          EndsWith(const AString& string) const;
+
+    _NODISCARD bool          EndsWithI(const char* other) const;
+    _NODISCARD bool          EndsWithI(const AString& other) const;
+
+    _NODISCARD bool          BeginsWith(char c) const;
+    _NODISCARD bool          BeginsWith(const char* string) const;
+    _NODISCARD bool          BeginsWith(const AString& string) const;
+
+    _NODISCARD bool          BeginsWithI(const char* string) const;
+    _NODISCARD bool          BeginsWithI(const AString& string) const;
+
+    _NODISCARD static bool   Match(const char* pattern, const char* string);
+    _NODISCARD bool          Matches(const char* pattern) const;
+    _NODISCARD bool          Matches(const AString& pattern) const;
+    _NODISCARD static bool   MatchI(const char* pattern, const char* string);
+    _NODISCARD bool          MatchesI(const char* pattern) const;
+    _NODISCARD bool          MatchesI(const AString& pattern) const;
+    _NODISCARD bool          MidStr(AString left, AString right, AString& Ret);
+    
+
+    static void                 Copy(const char* src, char* dst);
+    static void                 Copy(const char* src, char* dst, size_t len);
+
+    _NODISCARD static std::size_t    StrLen(const char* string);
+    _NODISCARD static std::int32_t   StrNCmp(const char* a, const char* b, size_t num);
+    _NODISCARD static std::int32_t   StrNCmpI(const char* a, const char* b, size_t num);
+
+
+    _NODISCARD static bool   IsWhitespace(char c);
+    _NODISCARD static bool   IsUppercaseLetter(char c);
+    _NODISCARD static bool   IsLowercaseLetter(char c);
+    _NODISCARD static bool   IsLetter(char c);
+    _NODISCARD static bool   IsNumber(char c);
+
+    static constexpr auto npos{ static_cast<std::uint32_t>(-1) };
+
+protected:
+    enum : std::uint32_t { 
+        MEM_MUST_BE_FREED_FLAG  = 0x00000001,
+        DO_NOT_FREE_MEMORY_FLAG = 0x80000020,
+        CAPACITY_MASK = 0x7FFFFFFF,
+        RESERVED_MASK = 0xFFFFFFFE
+    };
+
+    enum : std::uint64_t { 
+        k_offset_basis = 0xcbf29ce484222325ull,
+        k_prime = 1099511628211ull
+    };
+
+    void SetCapacity(std::uint32_t reserved, bool mustFreeMemory);
+    _NODISCARD std::uint32_t      get_capacity() const;
+    __declspec(noinline) void Grow(std::uint32_t new_length);
+    __declspec(noinline) void GrowNoCopy(std::uint32_t new_length); 
+    bool StrstrEx(const char* Str1, const char* Str2);
+
+    char*            str; 
+    std::uint32_t    intlength;
+    std::uint32_t    capacity;
+
+    static const char* const   s_EmptyString;
+    static const AString    s_EmptyAString;
+};
+
+template <std::uint32_t RESERVED = 256 >
+class AStackString : public AString{
+public:
+    explicit AStackString();
+    explicit AStackString(const AString& string);
+    explicit AStackString(AString&& string);
+    explicit AStackString(const AStackString& string);
+    explicit AStackString(AStackString&& string);
+    explicit AStackString(const char* string);
+    explicit AStackString(const char* start, const char* end);
+    inline ~AStackString() = default;
+
+    AStackString< RESERVED >& operator = (const char* string);
+    AStackString< RESERVED >& operator = (const AString& string);
+    AStackString< RESERVED >& operator = (AString&& string);
+    AStackString< RESERVED >& operator = (const AStackString& string);
+    AStackString< RESERVED >& operator = (AStackString&& string);
+
+private:
+    char _str[RESERVED + 1];
+};
 
 #endif
